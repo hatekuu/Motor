c67151e962b0cc8f955178278c5a4516fa0a98a1
hatekuu <hatekuu@gmail.com>
2024-02-25 07:46:45 +0700

    dong

--- app/getproducts/userinfor/page.js
+++ app/getproducts/userinfor/page.js
@@ -92,7 +92,7 @@ const router=useRouter();
   };
 
   return (
-    <div className="container mx-auto p-4 text-gray-950  bg-white">
+    <div className="container mx-auto p-4 text-gray-900 bg-white">
       <h1 className="text-2xl font-bold mb-4">User Information</h1>
 
       {/* Render full name field */}
